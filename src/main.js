61384db3658b0d82e8a190a58ada0e610a7f4500
DEWNS <bodiavanda@gmail.com>
2022-07-01 23:01:01 +0300

    Допилил навбар(поменять также верхний div слайдера)

--- src/main.js
+++ src/main.js
@@ -1,69 +1,119 @@
-$(function(){
-    $('.main_slider').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: true,
-        cssEase: 'linear',
-        speed: 1350,
-        draggable: false,
-        appendArrows: $('.arrows_sec'),
-        prevArrow: "<i class='fas fa-angle-left cursor-pointer group-hover:opacity-100 opacity-0 duration-500 float-left text-white text-2xl bg-black bg-opacity-50 hover:bg-opacity-100 ml-12 px-[14px] py-2'></i>",
-        nextArrow: "<i class='fas fa-angle-right cursor-pointer group-hover:opacity-100 opacity-0 duration-500 float-right text-white text-2xl bg-black bg-opacity-50 hover:bg-opacity-100 mr-12 px-[14px] py-2'></i>",
-        autoplay: true,
-        autoplaySpeed: 10000
-    })
+$(function () {
+  $('.main_slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    cssEase: 'linear',
+    speed: 1350,
+    draggable: false,
+    appendArrows: $('.arrows_sec'),
+    prevArrow: "<i class='fas fa-angle-left cursor-pointer group-hover:opacity-100 opacity-0 duration-500 float-left text-white text-2xl bg-black bg-opacity-50 hover:bg-opacity-100 ml-12 px-[14px] py-2'></i>",
+    nextArrow: "<i class='fas fa-angle-right cursor-pointer group-hover:opacity-100 opacity-0 duration-500 float-right text-white text-2xl bg-black bg-opacity-50 hover:bg-opacity-100 mr-12 px-[14px] py-2'></i>",
+    autoplay: true,
+    autoplaySpeed: 10000
+  })
 
-    $('.experts_slider').slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed: 200,
-        draggable: true,
-        arrows: false,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        pauseOnDotsHover: true,
-        pauseOnFocus: false
-    })
+  $('.experts_slider').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 200,
+    draggable: true,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnDotsHover: true,
+    pauseOnFocus: false
+  })
 
-    $('.achievements_slider').slick({
-      infinite: true,
-      slidesToShow: 4,
-      slidesToScroll: 3,
-      speed: 500,
-      draggable:  false,
-      dots: false,
-      autoplay: false,
-      arrows: true,
-      appendArrows: $('.ach_arrows'),
-      prevArrow: "<i class='fas fa-angle-left border-1 px-[7px] py-1 mr-1 text-gray-400 hover:text-black hover:border-[#f6ba18]'></i>",
-      nextArrow: "<i class='fas fa-angle-right border-1 px-[7px] py-1 text-gray-400 hover:text-black hover:border-[#f6ba18]'></i>",
-      pauseOnFocus: true
+  $('.achievements_slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    speed: 500,
+    draggable: false,
+    dots: false,
+    autoplay: false,
+    arrows: true,
+    appendArrows: $('.ach_arrows'),
+    prevArrow: "<i class='fas fa-angle-left border-1 px-[7px] py-1 mr-1 text-gray-400 hover:text-black hover:border-[#f6ba18]'></i>",
+    nextArrow: "<i class='fas fa-angle-right border-1 px-[7px] py-1 text-gray-400 hover:text-black hover:border-[#f6ba18]'></i>",
+    pauseOnFocus: true
   })
 
 
-    
+  //navbar group hovering
+  /*$('.bg-last-but').css('background-color', '#F6BB18');
+  $('.last-but').css('background-color', '#F6BB18');
+  $('.last-but').css('color', 'black');
+
+  
+  $('.bg-last-but').on('mouseover', function(){
+    $('.bg-last-but').css('background-color', 'black');
+    $('.last-but').css('background-color', 'black');
+    $('.last-but').af
+    $('.last-but').css('color', 'white');
+  })
+
+  $('.bg-last-but').on('mouseout', function(){
+    $('.bg-last-but').css('background-color', '#F6BB18');
+    $('.last-but').css('background-color', '#F6BB18');
+    $('.last-but').css('color', 'black');
+  })
+
+  $('.last-but').on('mouseover', function(){
+    $('.last-but').css('background-color', 'black');
+    $('.bg-last-but').css('background-color', 'black');
+    $('.last-but').css('color', 'white');
+  })
+
+  $('.last-but').on('mouseout', function(){
+    $('.last-but').css('background-color', '#F6BB18');
+    $('.bg-last-but').css('background-color', '#F6BB18');
+    $('.last-but').css('color', 'black');
+  })*/
+
+  $('.bg-last-but').on('mouseover', function(){
+    $('.bg-last-but').addClass('hovered');
+    $('.last-but').addClass('hovered');
+  })
+
+  $('.bg-last-but').on('mouseout', function(){
+    $('.bg-last-but').removeClass('hovered');
+    $('.last-but').removeClass('hovered');
+  })
+
+  $('.last-but').on('mouseover', function(){
+    $('.bg-last-but').addClass('hovered');
+    $('.last-but').addClass('hovered');
+  })
+
+  $('.last-but').on('mouseout', function(){
+    $('.bg-last-but').removeClass('hovered');
+    $('.last-but').removeClass('hovered');
+  })
+
 })
 
 //about us - tabs
 function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-      tabcontent[i].className = tabcontent[i].className.replace(" fade", "");
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-      
-    }
-    document.getElementById(tabName).style.display = "block";
-    document.getElementById(tabName).className += " fade";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    tabcontent[i].className = tabcontent[i].className.replace(" fade", "");
   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+
+  }
+  document.getElementById(tabName).style.display = "block";
+  document.getElementById(tabName).className += " fade";
+  evt.currentTarget.className += " active";
+}
 
 
 //counter observer
@@ -72,7 +122,7 @@ var options = {
   rootMargin: '0px',
   threshold: 1.0
 }
-var callback = function(entries, observer) {
+var callback = function (entries, observer) {
   $('.timer').countTo();
 };
 
@@ -80,3 +130,5 @@ var observer = new IntersectionObserver(callback, options);
 
 var target = document.querySelector('.timer');
 observer.observe(target);
+
+
--- src/navbar.js
+++ src/navbar.js
@@ -3,6 +3,7 @@ window.onscroll = function() {myFunction()};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
+var back_nav = document.getElementById("back-nav");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
@@ -11,7 +12,18 @@ var sticky = navbar.offsetTop;
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
+
+    back_nav.classList.add("sticky")
+    back_nav.classList.add("flex")
+    back_nav.classList.remove("hidden");
+    //back_nav.classList.replace(" hidden"," flex")
+
   } else {
     navbar.classList.remove("sticky");
+
+    back_nav.classList.remove("sticky");
+    back_nav.classList.add("hidden")
+    back_nav.classList.remove("flex");
+    //back_nav.classList.replace(" flex", " hidden")
   }
 }
